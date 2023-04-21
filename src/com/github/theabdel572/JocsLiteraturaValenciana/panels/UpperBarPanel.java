c357a807371d4f758ff86824acab468b32cede66
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-04-21 22:54:47 +0200

    Made the upper panel to implement the new interface and created getters to have more access to class properties from the components.

--- src/com/github/theabdel572/JocsLiteraturaValenciana/panels/UpperBarPanel.java
+++ src/com/github/theabdel572/JocsLiteraturaValenciana/panels/UpperBarPanel.java
@@ -5,12 +5,25 @@ import com.github.theabdel572.JocsLiteraturaValenciana.panels.components.Gamemod
 import javax.swing.*;
 import java.awt.*;
 
-public class UpperBarPanel extends JPanel {
-    public UpperBarPanel() {
+public class UpperBarPanel extends JPanel implements AppPanels{
+    private final JFrame frame;
+
+    public UpperBarPanel(JFrame frame) {
+        this.frame = frame;
+        //Setting Properties
         setLayout(new FlowLayout(FlowLayout.LEFT, 10, 10));
         setBackground(Color.GREEN);
-        add(new GamemodesComboBox());
+        //Adding Components
+        add(new GamemodesComboBox(this));
     }
 
+    @Override
+    public JPanel getPanel() {
+        return this;
+    }
 
+    @Override
+    public JFrame getParentFrame() {
+        return frame;
+    }
 }
