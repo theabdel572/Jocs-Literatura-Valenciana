9a4b4818e34c69f8ad655ee9b4363b580cd550fd
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-04-23 17:00:11 +0200

    Finished main game functionality.

--- src/com/github/theabdel572/JocsLiteraturaValenciana/panels/MemoryCardsPanel.java
+++ src/com/github/theabdel572/JocsLiteraturaValenciana/panels/MemoryCardsPanel.java
@@ -8,6 +8,8 @@ import java.util.*;
 import java.util.List;
 
 public class MemoryCardsPanel extends JPanel implements AppPanels{
+    private CardButton[] pairClickedCards = new CardButton[2];
+    private String[] pairClickedCardsText = new String[2];
     private final Map<String, String> cardRelations = Map.ofEntries(
             Map.entry("a", "b"),
             Map.entry("c", "d"),
@@ -27,11 +29,57 @@ public class MemoryCardsPanel extends JPanel implements AppPanels{
     }
 
 
+    // Execute the proper actions when a correct pair of cards is clicked.
+    public void setPairCompleted(){
+        for(CardButton cardButton : getPairClickedCards()){
+            cardButton.setEnabled(false);
+            cardButton.setPaired(true);
+            cardButton.setBackground(Color.GREEN);
+            cardButton.setAction(null);
+        }
+    }
+
+    // Reset the CardButton and String arrays that store the clicked cards.
+    public void resetCardButtonArrays(){
+        resetPairClickedCards();
+        resetPairClickedCardsText();
+    }
+
+    // Disable all the CardButtons in the panel.
+    public void disableCardButtons(){
+        Arrays.stream(getComponents()).iterator().forEachRemaining(component -> {
+            if(component instanceof CardButton){
+                component.setEnabled(false);
+            }
+        });
+    }
+
+    // Shuffle the cards.
     private List<String> shuffleCards(){
         List<String> keys = new ArrayList<>(cardRelations.keySet());
         keys.addAll(cardRelations.values());
         Collections.shuffle(keys);
         return keys;
+    }
+    
+    public String[] getPairClickedCardsText() {
+        return pairClickedCardsText;
+    }
+
+    public void resetPairClickedCardsText() {
+        this.pairClickedCardsText = new String[2];
+    }
+
+    public CardButton[] getPairClickedCards() {
+        return pairClickedCards;
+    }
+
+    public void resetPairClickedCards() {
+        this.pairClickedCards = new CardButton[2];
+    }
+
+    public Map<String, String> getCardRelations() {
+        return cardRelations;
     }
 
     public int getCardClicks() {
--- src/com/github/theabdel572/JocsLiteraturaValenciana/panels/components/CardButton.java
+++ src/com/github/theabdel572/JocsLiteraturaValenciana/panels/components/CardButton.java
@@ -6,8 +6,10 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
+import java.util.Map;
 
 public class CardButton extends JButton {
+    private boolean isPaired = false;
     private final String text;
     private final MemoryCardsPanel parentPanel;
 
@@ -19,27 +21,78 @@ public class CardButton extends JButton {
         addActionListener(new Listener());
     }
 
-    private class Listener implements ActionListener {
+    public void setPaired(boolean paired) {
+        isPaired = paired;
+    }
 
+    public boolean isPaired() {
+        return isPaired;
+    }
+
+    // The actions to execute when the player clicks the second button when trying to pair two cards.
+    private void onSecondButtonClickedActions(Timer timer){
+        parentPanel.disableCardButtons();
+
+        timer.setRepeats(false);
+        timer.start();
+
+        parentPanel.resetCardButtonArrays();
+    }
+
+    private class Listener implements ActionListener {
         // A timer that iterates over all the buttons in the parent panel and sets their text to "".
         // It also resets the card clicks to 0.
-        Timer timer = new Timer(2000, e1 -> {
+        Timer timer = new Timer(1500, actions -> {
             Arrays.stream(parentPanel.getComponents()).iterator().forEachRemaining(component -> {
-                if(component instanceof CardButton){
-                    ((CardButton) component).setText("");
+                if(!(component instanceof CardButton)){
+                    return;
+                }
+                ((CardButton) component).setText("");
+
+                if(!((CardButton) component).isPaired()){
+                    component.setEnabled(true);
                 }
             });
             parentPanel.resetCardClicks();
         });
 
         public void actionPerformed(ActionEvent e) {
-            if(parentPanel.getCardClicks() == 1){
-                timer.setRepeats(false);
-                timer.start();
-            }
             if(parentPanel.getCardClicks() < 2){
                 setText(text);
                 parentPanel.addCardClick();
+            }
+
+            switch (parentPanel.getCardClicks()) {
+                case 1 -> {
+                    parentPanel.getPairClickedCardsText()[0] = text;
+                    parentPanel.getPairClickedCards()[0] = (CardButton) e.getSource();
+                }
+                case 2 -> {
+                    parentPanel.getPairClickedCardsText()[1] = text;
+                    parentPanel.getPairClickedCards()[1] = (CardButton) e.getSource();
+
+                    for (Map.Entry<String, String> entry : parentPanel.getCardRelations().entrySet()) {
+                        if (entry.getValue().equals(parentPanel.getPairClickedCardsText()[0])) {
+                            String key = entry.getKey();
+                            if(key.equals(parentPanel.getPairClickedCardsText()[1])){
+                                parentPanel.setPairCompleted();
+                                onSecondButtonClickedActions(timer);
+                                return;
+                            }
+
+                            onSecondButtonClickedActions(timer);
+                            return;
+                        }
+                    }
+
+                    if(parentPanel.getCardRelations().get(parentPanel.getPairClickedCardsText()[0]).equals(parentPanel.getPairClickedCardsText()[1])){
+                        parentPanel.setPairCompleted();
+                        onSecondButtonClickedActions(timer);
+                        return;
+                    }
+
+                    onSecondButtonClickedActions(timer);
+                }
             }
         }
     }
