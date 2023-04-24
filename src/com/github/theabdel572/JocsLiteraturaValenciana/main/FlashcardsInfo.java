cd96794d64efdb0d5a7694bc9607c9127889e29a
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-04-24 22:17:42 +0200

    Created an enum to manage flashcards information.

---
+++ src/com/github/theabdel572/JocsLiteraturaValenciana/main/FlashcardsInfo.java
@@ -0,0 +1,12 @@
+package com.github.theabdel572.JocsLiteraturaValenciana.main;
+
+public enum FlashcardsInfo {
+    POMPEU_FABRA(new String[]{"test", "test"});
+
+    FlashcardsInfo(String[] info) {
+    }
+
+    public String[] getInfo() {
+        return new String[]{};
+    }
+}
