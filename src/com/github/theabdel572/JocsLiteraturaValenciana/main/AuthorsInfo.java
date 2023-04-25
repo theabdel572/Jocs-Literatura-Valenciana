e39d3a4caa0a5188ee5c0af1b33ccedd6a2c0a63
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-04-25 23:51:00 +0200

    Added the first author info to test all works properly.

--- src/com/github/theabdel572/JocsLiteraturaValenciana/main/AuthorsInfo.java
+++ src/com/github/theabdel572/JocsLiteraturaValenciana/main/AuthorsInfo.java
@@ -1,7 +1,12 @@
 package com.github.theabdel572.JocsLiteraturaValenciana.main;
 
 public enum AuthorsInfo {
-    POMPEU_FABRA("Pompeu Fabra", new String[]{"test", "test"}, (short) 1920);
+    POMPEU_FABRA("Pompeu Fabra",
+            new String[]{"Va ser el principal artífex de la normativització de la llengua catalana moderna.",
+            "Va publicar diverses gramàtiques, diccionaris i manuals de la llengua catalana, basant-se en criteris científics i respectant la pronúncia i l’etimologia dels mots.",
+            "La seva obra més coneguda és el Diccionari general de la llengua catalana de 1932.",
+            "Va ser fundador de la Secció Filològica de l’IEC i president de la Unió Catalana de Federacions Esportives."},
+            (short) 1868);
 
     AuthorsInfo(String name, String[] info, short year) {
         this.info = info;
