4ae14022aa3fb23492ea0626728dec6596a1a04c
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-04-29 13:40:53 +0200

    Created a button to reset the memorycards panel.

---
+++ src/com/github/theabdel572/JocsLiteraturaValenciana/panels/components/ResetButton.java
@@ -0,0 +1,29 @@
+package com.github.theabdel572.JocsLiteraturaValenciana.panels.components;
+
+import com.github.theabdel572.JocsLiteraturaValenciana.main.MainFrame;
+import com.github.theabdel572.JocsLiteraturaValenciana.panels.MemoryCardsPanel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ResetButton extends JButton{
+    private final MainFrame parentFrame;
+
+    public ResetButton(MainFrame parentFrame) {
+        super("Reset");
+        this.parentFrame = parentFrame;
+        addActionListener(new ResetButtonListener());
+    }
+
+    private class ResetButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(!(parentFrame.getCenterPanel() instanceof MemoryCardsPanel)){
+                return;
+            }
+
+            parentFrame.setCenterPanel(new MemoryCardsPanel(parentFrame));
+        }
+    }
+}
