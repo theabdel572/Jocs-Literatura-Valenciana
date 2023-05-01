435c7cee11ab990127cc392b92638ff7184b9dc4
theabdel572 <abdelkarimachbaniabid724@gmail.com>
2023-05-01 02:55:15 +0200

    Created a new whole class to design more visual buttons.

---
+++ src/com/github/theabdel572/JocsLiteraturaValenciana/panels/components/designs/ButtonDesign.java
@@ -0,0 +1,116 @@
+package com.github.theabdel572.JocsLiteraturaValenciana.panels.components.designs;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class ButtonDesign extends JButton {
+    private boolean over;
+    private Color color;
+    private Color colorOver;
+    private Color colorClick;
+    private Color borderColor;
+    private int radius = 0;
+
+    public ButtonDesign() {
+        //  Init Color
+
+        setColor(Color.WHITE);
+        colorOver = new Color(179, 250, 160);
+        colorClick = new Color(152, 184, 144);
+        borderColor = new Color(30, 136, 56);
+        setContentAreaFilled(false);
+        //  Add event mouse
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent me) {
+                setBackground(colorOver);
+                over = true;
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+                setBackground(color);
+                over = false;
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+                setBackground(colorClick);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+                if (over) {
+                    setBackground(colorOver);
+                } else {
+                    setBackground(color);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        //  Paint Border
+        g2.setColor(borderColor);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2.setColor(getBackground());
+        //  Border set 2 Pix
+        g2.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, radius, radius);
+        super.paintComponent(grphcs);
+    }
+
+    public boolean isOver() {
+        return over;
+    }
+
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+        setBackground(color);
+    }
+
+    public Color getColorOver() {
+        return colorOver;
+    }
+
+    public void setColorOver(Color colorOver) {
+        this.colorOver = colorOver;
+    }
+
+    public Color getColorClick() {
+        return colorClick;
+    }
+
+    public void setColorClick(Color colorClick) {
+        this.colorClick = colorClick;
+    }
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(Color borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
